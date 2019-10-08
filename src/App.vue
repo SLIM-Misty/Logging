0bd639ab85d8db852e1f027b01e3c9374a8ef0f3
mcavoybn <mcavoybn@gmail.com>
2019-10-08 15:23:34 -0600

    added polling and save functionality to pictures page

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <span style="margin-right:15px">CS481 Misty Project</span>
         <router-link to="/" style="margin-left:15px">Logging</router-link>
         <router-link to="routine" style="margin-left:15px">Routine</router-link>
-        <router-link to="takepicture" style="margin-left:15px">Take Picture</router-link>
+        <router-link to="takepicture" style="margin-left:15px">Pictures</router-link>
       </v-toolbar-title>
     </v-app-bar>
 
--- src/views/TakePicture.vue
+++ src/views/TakePicture.vue
@@ -3,10 +3,17 @@
         <v-layout wrap>
             <v-flex xs4>
                 <v-text-field label="Bot IP" v-model="botIp" />
-                <v-btn :loading="loadingPicture" @click="takePicture()">Take Picture!</v-btn>
+                <v-btn :loading="loadingPicture" @click="start()">Start Recording</v-btn>
+                <v-btn v-if="!stopped" @click="stop()">Stop</v-btn>
+                <v-btn v-if="images.length > 0" @click="saveImages()">Download Images</v-btn>
             </v-flex>
             <v-flex xs8>
-                <img :src="imageSource" />
+                <img :src="selectedImage || images[0] && images[0].withHeader" />
+            </v-flex>
+        </v-layout>
+        <v-layout wrap>
+            <v-flex xs1 v-for="i in images" :key="i">
+                <img :src="i.withHeader" height="50px" width="50px" @click="selectedImage = i.withHeader"/>
             </v-flex>
         </v-layout>
     </v-container>
@@ -16,17 +23,48 @@
 import base64Img from 'base64-img'
 import uuid from 'uuid/v4'
 import fs from 'fs'
+import moment from 'moment'
+
+function base64toBlob(base64Data, contentType) {
+    contentType = contentType || '';
+    var sliceSize = 1024;
+    var byteCharacters = atob(base64Data);
+    var bytesLength = byteCharacters.length;
+    var slicesCount = Math.ceil(bytesLength / sliceSize);
+    var byteArrays = new Array(slicesCount);
+
+    for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+        var begin = sliceIndex * sliceSize;
+        var end = Math.min(begin + sliceSize, bytesLength);
+
+        var bytes = new Array(end - begin);
+        for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
+            bytes[i] = byteCharacters[offset].charCodeAt(0);
+        }
+        byteArrays[sliceIndex] = new Uint8Array(bytes);
+    }
+    return new Blob(byteArrays, { type: contentType });
+}
 
 export default {
     data() {
         return {
             botIp: "10.10.0.7",
-            imageSource: "",
-            loadingPicture: false
+            loadingPicture: false,
+            images: [],
+            stopped: false,
+            selectedImage: ""
         }
     },
     methods: {
-        takePicture () {
+        start() {
+            this.stopped = false;
+            this.takePictures();
+        },
+        stop() {
+            this.stopped = true;
+        },
+        takePictures () {
             this.loadingPicture = true;
             Promise.race([
             fetch(`http://${this.botIp}/api/cameras/rgb?base64=true&fileName=asdf&width=500&height=500&displayOnScreen=true&overwriteExisting=false`, {
@@ -37,18 +75,43 @@ export default {
         .then(response => response.json())
         .then(jsonData => {
             console.log(jsonData);
-            const imageBase64 = 'data:image/png;base64,' + jsonData.result.base64;
-            this.imageSource = imageBase64;
+            // the base64toBlob helper expects an encoding without the header
+            // so we just store both so they can be easily displayed
+            // and saved as well
+            const withoutHeader = jsonData.result.base64;
+            const withHeader = 'data:image/png;base64,' + jsonData.result.base64;
+            this.images.push({withoutHeader, withHeader});
             this.loadingPicture = false;
-            // base64Img.img(imageBase64, './images', uuid(), (arg) => {
-            //     console.log(arg);
-            // }); 
+            if (!this.stopped) {
+                setTimeout(this.takePictures, 1000);
+            }
         })
-        }
+        },
+        saveImages () {
+            this.images.forEach((image, index) => {
+                this.saveFile(image.withoutHeader, index + "-" + moment().format() + ".png");
+            });
+        },
+        saveFile(data, filename) {
+            var file = base64toBlob(data, "image/png");
+            if (window.navigator.msSaveOrOpenBlob)
+                // IE10+
+                window.navigator.msSaveOrOpenBlob(file, filename);
+            else {
+                // Others
+                var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+                a.href = url;
+                a.download = filename;
+                document.body.appendChild(a);
+                a.click();
+                setTimeout(function() {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+                }, 0);
+            }
+            },
     },
-    mounted()    {
-        
-    }
 
 }
 </script>
